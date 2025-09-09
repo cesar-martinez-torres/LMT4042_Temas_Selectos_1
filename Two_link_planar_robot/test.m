1f7b207a245a11b4702774ca951c9c9c9e53a27c
cesar-martinez-torres <cesar.martinez@udlap.mx>
2025-09-09 16:10:58 -0600

    9 septiembre 2025

--- Two_link_planar_robot/test.m
+++ Two_link_planar_robot/test.m
@@ -1,4 +1,13 @@
 syms th1 th2 L1 L2
-R = rotMatrix(th1);     % 30° rotation
-T = translMatrix([L1 0]);     % Translation of (5,2)
-H = T * R;                   % Full homogeneous transformation
+R1 = rotMatrix(th1,'deg');     % 30° rotation
+T1 = translMatrix([L1 0]);     % Translation of (5,2)
+H1 = R1 * T1;                   % Full homogeneous transformation
+R2 = rotMatrix(th2,'deg');     % 30° rotation
+T2 = translMatrix([L2 0]);     % Translation of (5,2)
+H2 = R2 * T2;                   % Full homogeneous transformation
+H= H1*H2
+th1=25
+th2=80
+L1=1.2
+L2=1.2
+Ht=eval(H)
---
+++ Two_link_planar_robot/two_link_planar.m
@@ -0,0 +1,15 @@
+%% Two Link Planar Robot Kinematic Model
+% Orientate correctly the links
+% Sizes (meters)
+%First Link
+L1_X=1.2
+L1_Y=0.1
+L1_Z=0.1
+L1_density=2700
+L1_color=[1 0 0]
+% Secon link
+L2_X=1.2
+L2_Y=0.1
+L2_Z=0.1
+L2_density=2700
+L2_color=[0 0 1]
