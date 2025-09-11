6de5772b5dff5199f04a50a3d474567890f3ab86
cesar-martinez-torres <cesar.martinez@udlap.mx>
2025-09-11 17:30:51 -0600

    11 septiembre 2025

--- Two_link_planar_robot/Gen_path.m
+++ Two_link_planar_robot/Gen_path.m
@@ -1,11 +1,11 @@
-tf=150 % Number of Path points
+tf=1500 % Number of Path points
 A=[tf^2 tf^3; 2*tf 3*tf^2]%Matrix A
 %% For "th1" 
-xd=10 % Desired final position in th1 
+xd=45 % Desired final position in th1 
 xpd=0 % Desired final velocity in th1
 a0=0  % Initial condition in th1 position
 a1=0  % Initial condition in th1 velocity
-B=[10;0]
+B=[xd;xpd]
 coef=(inv(A))*B
 a2=coef(1)
 a3=coef(2)
